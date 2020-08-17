961d3c542a6ea4f1de19ca4cb9373936722ba838
Eamonn Smyth <e.smyth75@gmail.com>
2020-08-17 20:50:30 +0000

    Add code to display messages from url

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 
 app = Flask(__name__)
+messages = []
+
+
+def add_messages(username, message):
+    """ Add messages to the messages list """
+    messages.append(f"{username}: {message}")
+
+
+def get_all_messages():
+    """ Get all the messages and separate them with the 'br' tag"""
+    return "<br>".join(messages)
 
 
 @app.route("/")
@@ -13,12 +24,15 @@ def index():
 
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """ Display a chat message """
+    return f"<h1>Welcome, {username}</h1>{get_all_messages()}"
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return f"{username} here is your message: {message}"
+    """Create a chat message and then redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
